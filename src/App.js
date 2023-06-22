a941da9870bd0383372f1d09d0e2fbf90e30e934
C28798 <narayan.choudhary@nl.abnamro.com>
2023-06-22 11:48:14 +0200

    frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataFetchingComponent from './DataFetchingComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetchingComponent />
+    </div>
+  );
+}
+
+export default App;
